d0ce4e8434bba87ee65db28c3bb38f35120e8800
Mauricio Graciano <mau.graci@gmail.com>
2021-04-20 16:24:43 -0500

    Added server endpoint to convert band colors to a label

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,10 +1,70 @@
 import express, { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+import colorCodes from './ColorCodes.json';
 
 var router = express.Router();
+
+interface ColorCode {
+  number: number | null;
+  exponent: number;
+  tolerance: number | null;
+}
+
+const ColorCodes: Record<string, ColorCode> = colorCodes;
+
+const ValidNumberColors = Object.keys(ColorCodes).filter((key) => ColorCodes[key].number !== null);
+const ValidToleranceColors: Array<string | null> = Object.keys(ColorCodes).filter(
+  (key) => ColorCodes[key].tolerance !== null
+);
+// Add empty (null) tolerance band to valid colors
+ValidToleranceColors.push(null);
 
 /* GET home page. */
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json(['respond with a resource']);
 });
+
+// POST band colors of a resistors to get the label associated to the bands
+router.post(
+  '/calculate-value',
+  body('firstBand').isIn(ValidNumberColors),
+  body('secondBand').isIn(ValidNumberColors),
+  body('exponentBand').isIn(Object.keys(ColorCodes)),
+  body('toleranceBand').exists().isIn(ValidToleranceColors),
+  (req: Request, res: Response, next: NextFunction) => {
+    // Finds the validation errors in this request
+    // and wraps them in an object.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // Only retrieve the names of the invalid params & make sure they are distinct.
+      let invalidFields = errors.array().map((error) => error.param);
+      invalidFields = Array.from(new Set(invalidFields));
+
+      // Return invalid fields
+      return res
+        .status(400)
+        .set('Content-Type', 'application/json; charset=utf-8')
+        .json({ invalidFields });
+    }
+    // If no errors occur, we can safely retrieve & manipulate the bands
+    const { exponentBand, firstBand, secondBand, toleranceBand } = req.body;
+
+    // Get the values associated to the colors of the passed bands
+    const { exponent } = ColorCodes[exponentBand];
+    const firstSignificantDigit = ColorCodes[firstBand].number ?? 1;
+    const secondSignificantDigit = ColorCodes[secondBand].number ?? 0;
+    const tolerance = toleranceBand === null ? 20 : ColorCodes[toleranceBand].tolerance ?? 20;
+
+    // Calculate the number according to digits & exponent bands
+    const number = (firstSignificantDigit * 10 + secondSignificantDigit) * Math.pow(10, exponent);
+
+    // Return label that corresponds to the labels.
+    res
+      .status(200)
+      .set('Content-Type', 'text/html; charset=utf-8')
+      .send(`${number} ohms ±${tolerance}%`);
+  }
+);
 
 export default router;
--- api/tests/index.test.ts
+++ api/tests/index.test.ts
@@ -2,9 +2,53 @@ import request from 'supertest';
 import app from '../app';
 
 describe('Test the root path', () => {
-  test('It should response the GET method', async () => {
+  test('It should respond to the GET method', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.body).toStrictEqual(['respond with a resource']);
+  });
+});
+
+describe('Test the calculate value path', () => {
+  test('It should respond to empty body sent to POST method', async () => {
+    const response = await request(app).post('/calculate-value');
+    expect(response.status).toBe(400);
+    expect(response.text).toBe(
+      JSON.stringify({
+        invalidFields: ['firstBand', 'secondBand', 'exponentBand', 'toleranceBand'],
+      })
+    );
+  });
+  test('It should respond to invalid color sent to POST method', async () => {
+    const response = await request(app)
+      .post('/calculate-value')
+      .set('Content-Type', 'application/json')
+      .send({
+        firstBand: 'transparent',
+        secondBand: 'red',
+        exponentBand: 'orange',
+        toleranceBand: 'gold',
+      })
+      .set('Accept', 'text/html');
+    expect(response.status).toBe(400);
+    expect(response.text).toBe(
+      JSON.stringify({
+        invalidFields: ['firstBand'],
+      })
+    );
+  });
+  test('It should respond with correct value sent to POST method', async () => {
+    const response = await request(app)
+      .post('/calculate-value')
+      .set('Content-Type', 'application/json')
+      .send({
+        firstBand: 'red',
+        secondBand: 'red',
+        exponentBand: 'orange',
+        toleranceBand: 'gold',
+      })
+      .set('Accept', 'text/html');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('22000 ohms ±5%');
   });
 });
