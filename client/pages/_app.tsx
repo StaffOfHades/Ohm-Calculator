dad3075aedab41bd211370c2b74a517cf5c248d4
Mauricio Graciano <mau.graci@gmail.com>
2021-04-21 12:31:36 -0500

    Fixed issue with client app where viewport meta content was not properly defined

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
+import { Fragment } from 'react';
+import Head from 'next/head';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../styles/globals.sass';
 
 export default function MainApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
